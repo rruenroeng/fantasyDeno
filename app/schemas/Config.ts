4acb038898708119f882345550e2450849f78fc6
Ryan Ruenroeng <rruenroeng@gmail.com>
2022-08-07 11:24:17 -0500

    Centralizing configuration/doing TS things.

---
+++ app/schemas/Config.ts
@@ -0,0 +1,47 @@
+import { z } from "zod";
+
+import EnvironmentVariableNames from "../constants/EnvironmentVariableNames.ts";
+
+function getErrorMessage(environmentVariableName: EnvironmentVariableNames) {
+  return {
+    message: `Missing ${environmentVariableName}) environment variable.`,
+  };
+}
+
+//TODO: Learn about zod
+export const ConfigSchema = z.object({
+  base_url: z
+    .string()
+    .min(1, getErrorMessage(EnvironmentVariableNames.BASE_URL)),
+  environment: z
+    .string()
+    .min(1, getErrorMessage(EnvironmentVariableNames.DENO_ENV)),
+  db: z.object({
+    host: z.string().min(1, getErrorMessage(EnvironmentVariableNames.DB_HOST)),
+    username: z
+      .string()
+      .min(1, getErrorMessage(EnvironmentVariableNames.DB_USERNAME)),
+    password: z
+      .string()
+      .min(1, getErrorMessage(EnvironmentVariableNames.DB_PASSWORD)),
+    database: z
+      .string()
+      .min(1, getErrorMessage(EnvironmentVariableNames.DB_NAME)),
+    port: z.number().default(5432),
+  }),
+  oauth: z.object({
+    discord: z.object({
+      client_id: z
+        .string()
+        .min(1, getErrorMessage(EnvironmentVariableNames.DISCORD_CLIENT_ID)),
+      client_secret: z
+        .string()
+        .min(
+          1,
+          getErrorMessage(EnvironmentVariableNames.DISCORD_CLIENT_SECRET)
+        ),
+    }),
+  }),
+});
+
+export type Config = z.infer<typeof ConfigSchema>;
--- app/utils/config.ts
+++ app/utils/config.ts
@@ -12,22 +12,23 @@ import { Config, ConfigSchema } from "@/schemas/Config.ts";
 const envConfig: Config = {
   base_url:
     Deno.env.get(EnvironmentVariableNames.BASE_URL) || "https://localhost:8000",
-    environment: Deno.env.get(EnvironmentVariableNames.DENO_ENV) || "".
-    db: {
-        database: Deno.env.get(EnvironmentVariableNames.DB_NAME) || "",
-        host: Deno.env.get(EnvironmentVariableNames.DB_HOST) || "".
-        username: Deno.env.get(EnvironmentVariableNames.DB_USERNAME) || "",
-        password: Deno.env.get(EnvironmentVariableNames.DB_PASSWORD) || "",
-        port: Number(Deno.env.get(EnvironmentVariableNames.DB_PORT) || 5432),
-    }
-    oauth: {
-        discord: {
-            client_id: Deno.env.get(EnvironmentVariableNames.DISCORD_CLIENT_ID) || "",
-            client_secret: Deno.env.get(EnvironmentVariableNames.DISCORD_CLIENT_SECRET) || "",
-        },
+  environment: Deno.env.get(EnvironmentVariableNames.DENO_ENV) || "",
+  db: {
+    database: Deno.env.get(EnvironmentVariableNames.DB_NAME) || "",
+    host: Deno.env.get(EnvironmentVariableNames.DB_HOST) || "",
+    username: Deno.env.get(EnvironmentVariableNames.DB_USERNAME) || "",
+    password: Deno.env.get(EnvironmentVariableNames.DB_PASSWORD) || "",
+    port: Number(Deno.env.get(EnvironmentVariableNames.DB_PORT) || 5432),
+  },
+  oauth: {
+    discord: {
+      client_id: Deno.env.get(EnvironmentVariableNames.DISCORD_CLIENT_ID) || "",
+      client_secret:
+        Deno.env.get(EnvironmentVariableNames.DISCORD_CLIENT_SECRET) || "",
     },
+  },
 };
 
-const config = ConfigSchema.parse(envConfig)
+const config = ConfigSchema.parse(envConfig);
 
 export default config;
